c4cd98613e502c5c2861c454edf32386953c9c47
Júlia Tonieto <juxtonie@hotmail.com>
2023-11-24 21:32:10 -0300

    subindo tests

--- scenarios/index.js
+++ scenarios/index.js
@@ -9,30 +9,10 @@ export let getCustomerSuccessRate = new Rate('get_customer_success_rate');
 export let getCustomerReqs = new Rate('get_customer_reqs');
 
 export default function () {
-    const url = 'http://127.0.0.1:5000/signup_developer'; // Substitua pela URL real da sua API
-    const headers = { 'Content-Type': 'application/json' };
-    const payload = JSON.stringify({
-            tipoUsuario: "desenvolvedor",
-            email: "teste@email.com",
-            senha: "1222212121",
-            telefone: "212323224002",
-            nome: "Usuario Teste Connect",
-            genero: "Feminino",
-            data_nascimento: "30/10/2001",
-            cpf: "01234567811",
-            habilidades: ["JavaScript", "Python"],
-            experiencia: "10 anos",
-            tag_desenvolvedor: ["backend", "IA"],
+   http.get('http://127.0.0.1:5000/signup_dev');
 
-    });
-  
-    const response = http.post(url, payload, { headers });
-  
-    // Verificar o código de status da resposta
-    check(response, {
-      'status is 201': (r) => r.status === 201,
-    });
-  
+    //exec('python teste.py');
+    sleep(25);
 }
 
 
--- scenarios/post.js
+++ scenarios/post.js
@@ -1,21 +1,29 @@
+import http from 'k6/http';
+import { check, fail } from 'k6';
+import { sleep } from 'k6';
+import { Trend, Rate } from 'k6/metrics';
+
+export let getCustomerDuration = new Trend('get_customer_duration');
+export let getCustomerFailRate = new Rate('get_customer_fail_rate');
+export let getCustomerSuccessRate = new Rate('get_customer_success_rate');
+export let getCustomerReqs = new Rate('get_customer_reqs');
+
 export default function () {
-    const url = 'https://exemplo.com/api/usuarios'; // Substitua pela URL real da sua API
+    const url = 'http://127.0.0.1:5000/signup_developer'; // Substitua pela URL real da sua API
     const headers = { 'Content-Type': 'application/json' };
     const payload = JSON.stringify({
+            tipoUsuario: "desenvolvedor",
+            email: "teste@email.com",
+            senha: "1222212121",
+            telefone: "212323224002",
+            nome: "Usuario Teste Connect",
+            genero: "Feminino",
+            data_nascimento: "30/10/2001",
+            cpf: "01234567811",
+            habilidades: ["JavaScript", "Python"],
+            experiencia: "10 anos",
+            tag_desenvolvedor: ["backend", "IA"],
 
-            "tipoUsuario": "desenvolvedor",
-            "email": "teste@email.com",
-            "senha": "1222212121",
-            "telefone": "212323224002",
-            "nome": "Usuario Teste Connect",
-            "genero": "Feminino",
-            "data_nascimento": "30/10/2001",
-            "cpf": "01234567811",
-            "habilidades": ["JavaScript", "Python"],
-            "experiencia": "10 anos",
-            "tag_desenvolvedor": ["backend", "IA"],
-            "status": True
-        }
     });
   
     const response = http.post(url, payload, { headers });
@@ -25,11 +33,4 @@ export default function () {
       'status is 201': (r) => r.status === 201,
     });
   
-    // Adicionar mais verificações conforme necessário
-    // Exemplo: Verificar o conteúdo da resposta
-    check(response, {
-      'response contains success': (r) => r.body.includes('success'),
-    });
-  
-    sleep(1); // Adiciona um segundo de pausa entre as solicitações
-  }
+}
